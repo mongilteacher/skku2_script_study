9ed7eec289882d278e28b0eae53b023ac8390125
mongilteacher <158262352+mongilteacher@users.noreply.github.com>
2025-10-27 16:07:12 +0900

    조건문

---
+++ Example.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Example : MonoBehaviour
+{
+    void Start()
+    {
+        // 입력 받았다고 가정..
+        int a = 1;
+        int b = 2;
+
+        int result = a + b;
+        Debug.Log(result);
+
+        Debug.Log(a + b);
+    }
+}
